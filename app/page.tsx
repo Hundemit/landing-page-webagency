2c8b536e868dc911558a1ac3bcb634ddba2f33ca
Jan Hindemit <janhindemit1@gmail.com>
2025-09-16 13:42:04 +0200

    Enhance homepage layout and configuration: Added a new Hero2 component and LogosMarquee for improved visual appeal, updated the Navbar for better responsiveness, configured image domains in next.config.ts, and commented out the previous Hero component in the main page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 import { CallToAction } from "@/components/ui/CallToAction";
 import FeatureDivider from "@/components/ui/FeatureDivider";
 import { Hero } from "@/components/ui/Hero";
+import { Hero2 } from "@/components/ui/Hero2";
+
 import { Map } from "@/components/ui/Map/Map";
 import { SolarAnalytics } from "@/components/ui/SolarAnalytics";
 import Features05Page from "@/components/features-05/features-05";
 import Footer from "@/components/ui/Footer";
 import { AboutUs } from "@/components/ui/about-us";
+import LogosMarquee from "@/components/LogosMarquee/logos-marquee";
 
 export default function Home() {
   return (
     <main className="relative mx-auto flex flex-col">
-      <Hero className="" />
-
+      {/* <Hero className="" /> */}
+      <Hero2 className="" />
+      <div className=" h-20 flex items-center justify-center">
+        <LogosMarquee className=" w-full" />
+      </div>
       {/* space for the navbar */}
       <div className="h-32"></div>
 
---
+++ components/ui/Hero2.tsx
@@ -0,0 +1,42 @@
+import { RiArrowRightUpLine } from "@remixicon/react";
+import { FadeDiv, FadeSpan } from "@/components/Fade";
+
+import GameOfLife from "./HeroBackground";
+import { cn } from "@/lib/utils";
+import LogosMarquee from "../LogosMarquee/logos-marquee";
+import { Sparkles } from "lucide-react";
+import { LinkButton } from "./LinkButton";
+import Image from "next/image";
+
+export function Hero2({ className }: { className?: string }) {
+  return (
+    <section aria-label="hero" className={cn("relative flex flex-col items-center   h-screen min-h-[424px] pt-22 md:pt-32 duration-300 ", className)}>
+      <div className="flex flex-col sm:flex-row justify-between w-full max-w-7xl items-end px-4  h-fit sm:min-h-[424px] min-h-[409px]">
+        {/* left side */}
+        <div className="space-y-4 mb-8 md:mb-32">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold duration-500">
+            <FadeSpan>Digital</FadeSpan> <FadeSpan>noch</FadeSpan>
+            <br />
+            <FadeSpan>mehr</FadeSpan> <FadeSpan>erreichen.</FadeSpan>
+          </h1>
+          <div className="w-fit max-w-xl text-base sm:text-base md:text-lg text-balance duration-500">
+            <FadeSpan>Unternehmen brauchen vor allem eins: </FadeSpan>
+            <FadeSpan>echte Kundenbeziehungen und nachhaltigen Wachstum!</FadeSpan>
+          </div>
+          <LinkButton variant="light" className="" text="Contact Us" href="#" />
+        </div>
+
+        {/* right side */}
+        <div className=" w-3/5  sm:w-72 rounded-xl overflow-hidden -mb-12 md:-mb-18   z-10 duration-300  shadow-xl">
+          <div className="w-full h-32 md:h-64 bg-gray-700 duration-300"></div>
+          <div className="w-full h-12 md:h-18 bg-primary duration-300"></div>
+        </div>
+      </div>
+
+      {/* Img Section */}
+      <div className="bg-red-300 w-full h-full overflow-hidden min-h-[200px] bg-[url('https://cdn.prod.website-files.com/684a11845c7dd0b4b7745cd9/684f57a20f6e64c09db89289_Celie-1-1170x780.webp')] bg-cover bg-center">
+        {/* <img src="https://cdn.prod.website-files.com/684a11845c7dd0b4b7745cd9/684f57a20f6e64c09db89289_Celie-1-1170x780.webp" alt="hero2" className="" /> */}
+      </div>
+    </section>
+  );
+}
